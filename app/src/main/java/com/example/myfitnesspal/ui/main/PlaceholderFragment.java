8fa7519afbc236b08849198af8e64fbf6dd113ad
Shrestha <shresthabanerjee@gmail.com>
2021-12-16 00:40:03 +0530

    new activity

--- app/src/main/java/com/example/myfitnesspal/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/myfitnesspal/ui/main/PlaceholderFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.myfitnesspal.R;
-import com.example.myfitnesspal.databinding.FragmentChooseBinding;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,8 +19,7 @@ public class PlaceholderFragment extends Fragment {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
-    private PageViewModel pageViewModel;
-private FragmentChooseBinding binding;
+
 
     public static PlaceholderFragment newInstance(int index) {
         PlaceholderFragment fragment = new PlaceholderFragment();
@@ -31,38 +29,5 @@ private FragmentChooseBinding binding;
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
-        int index = 1;
-        if (getArguments() != null) {
-            index = getArguments().getInt(ARG_SECTION_NUMBER);
-        }
-        pageViewModel.setIndex(index);
-    }
 
-    @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-
-      binding = FragmentChooseBinding.inflate(inflater, container, false);
-      View root = binding.getRoot();
-
-        final TextView textView = binding.sectionLabel;
-        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
-
-@Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
