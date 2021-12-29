ee40b2858dc11fea82fea09ce3c60be42aae2f28
Shrestha <shresthabanerjee@gmail.com>
2021-12-30 01:06:41 +0530

    new activities

--- app/src/androidTest/java/com/example/myfitnesspal/StepCounterActivity.java
+++ app/src/androidTest/java/com/example/myfitnesspal/StepCounterActivity.java
@@ -1,4 +1,21 @@
 package com.example.myfitnesspal;
 
-public class StepCounterActivity {
+import android.app.Activity;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class StepCounterActivity extends Activity implements SensorEventListener{
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Sensor isBooleanPresent;
+    private TextView mStepsSinceReboot;
+
+    @Overrideprotected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.stepcounter_layout);
+
+        mStepsSinceReboot = (TextView)findViewById(R.id.stepssincerebooot);
+        mSensorManager = (Se)
+    }
 }
