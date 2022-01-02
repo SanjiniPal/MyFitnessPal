6049133c5add1b9333a2ae00a5200dee31664fda
Shrestha <shresthabanerjee@gmail.com>
2022-01-02 20:28:57 +0530

    new activity BMI

--- app/src/main/java/com/example/myfitnesspal/BMIActivity.java
+++ app/src/main/java/com/example/myfitnesspal/BMIActivity.java
@@ -3,6 +3,9 @@ package com.example.myfitnesspal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BMIActivity extends AppCompatActivity {
 
@@ -10,5 +13,46 @@ public class BMIActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmiactivity);
+
+        EditText edWeg,edHei;
+        TextView txtRes,txtInter;
+        Button btnRes,btnReset;
+
+        edWeg = (EditText) findViewById(R.id.edweg);
+        edHei = (EditText) findViewById(R.id.edhei);
+
+        txtInter = (TextView) findViewById(R.id.txtinter);
+        txtRes = (TextView) findViewById(R.id.txtres);
+
+        btnRes = (Button) findViewById(R.id.btnres);
+        btnReset = (Button) findViewById(R.id.btnreset);
+
+    }
+    public float BMICalculate(float weight, float height)
+    {
+        return weight/(height * height);
+    }
+    public String interpretBMI(float bmiValue)
+    {
+        if(bmiValue < 16)
+        {
+            return "Severely Underweight";
+        }
+        else if(bmiValue < 18.5)
+        {
+            return "Underweight";
+        }
+        else if(bmiValue < 25)
+        {
+            return "Normal";
+        }
+        else if(bmiValue < 25)
+        {
+            return "Overweight";
+        }
+        else
+        {
+            return "Obese";
+        }
     }
 }
